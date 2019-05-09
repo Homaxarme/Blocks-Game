474b80ecfde3259b4b110c0c46c7e112f3fae800
Homaxarme <gdicuba@gmail.com>
2019-05-08 20:06:26 -0400

    Add Project

---
+++ Blocks/Assets/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform PlayerOne;
+    public Transform PlayerTwo;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerOne.position.y < -1)
+        {
+            Restart();
+        }
+        if (PlayerTwo.position.y <-1)
+        {
+            Restart();
+        }
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Blocks/Assets/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public bool playerOne;
+    public Rigidbody rigidbody;
+    public float force;
+    void FixedUpdate()
+        {
+        if(playerOne)
+        {
+            if (Input.GetKey("w"))
+            {
+                rigidbody.AddForce(0, 0, force * Time.deltaTime,ForceMode.VelocityChange);
+            }
+            if (Input.GetKey("s"))
+            {
+                rigidbody.AddForce(0, 0, -force * Time.deltaTime,ForceMode.VelocityChange);
+            }
+            if (Input.GetKey("d"))
+            {
+                rigidbody.AddForce(force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            }
+            if (Input.GetKey("a"))
+            {
+                rigidbody.AddForce(-force* Time.deltaTime, 0, 0,ForceMode.VelocityChange);
+            }
+        }
+        else
+        {
+            if (Input.GetKey("up"))
+            {
+                rigidbody.AddForce(0, 0, force * Time.deltaTime, ForceMode.VelocityChange);
+            }
+            if (Input.GetKey("down"))
+            {
+                rigidbody.AddForce(0, 0, -force * Time.deltaTime, ForceMode.VelocityChange);
+            }
+            if (Input.GetKey("right"))
+            {
+                rigidbody.AddForce(force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            }
+            if (Input.GetKey("left"))
+            {
+                rigidbody.AddForce(-force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            }
+        }
+	}
+}
